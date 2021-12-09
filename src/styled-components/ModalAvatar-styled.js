3fd451cff49dca3f0222db8e19152001ff655a10
dafiqarba <daviqarba@live.com>
2021-12-09 16:23:27 +0700

    Implement circular avatar container and update the demo in readme

--- src/styled-components/ModalAvatar-styled.js
+++ src/styled-components/ModalAvatar-styled.js
@@ -4,17 +4,26 @@ export const Wrapper = styled.div`
   background-color: #5bc6ac;
   width: 35%;
   border-radius: 1rem 0 0 1rem;
-`;
-
-export const ContentWrapper = styled.div`
-  width: 100%;
-  height: 100%;
 
   display: flex;
   align-items: center;
   justify-content: center;
 `;
 
+export const ContentWrapper = styled.div`
+  background-color:#f9f9f9;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  width: 17rem;
+  height: 17rem;
+  border-radius: 50%;
+
+  overflow: hidden;
+  border: 0.3rem solid #f9f9f9; 
+`;
+
 export const Avatar = styled.img`
-  width: 70%;
+height: 100%;
 `;
